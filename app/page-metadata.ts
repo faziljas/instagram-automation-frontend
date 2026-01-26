812ebdcf03e1c3844ad2bd2c21d6430b81378887
Mohamed Fazil Sirajudeen <mohamedfazilsirajudeen@Mohameds-MacBook-Pro.local>
2026-01-26 20:39:08 +0800

    Remove incorrect page-metadata.ts file

--- app/page-metadata.ts
+++
@@ -1,8 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: {
-    absolute: 'LogicDM',
-  },
-  description: 'Automate your Instagram marketing and engagement with powerful automation rules, scheduled posts, and analytics.',
-};
